7264f87df34fb526e1ffa9d39bb1c702314b6854
Dan Huckson <danhuckson@hotmail.com>
2020-06-15 21:49:57 -0400

    The start

---
+++ tree.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+
+data = {
+    'n1': {
+        'text': 'text n1',
+        'i1': {'text': 'test i 1'}
+    },
+    'n2': {
+        'text': 'text n1',
+        'i1': {'text': 'test i 1'}
+    },
+}
+
+que = deque()
+for k, v in data.items():
+    que.append({k:v})
+
+for idx, item in enumerate(que):
+    print(idx, item)
+
