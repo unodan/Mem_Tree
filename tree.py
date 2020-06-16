976b430bbaca6087a7702acbd0c9d75678bad523
dan <danhuckson@hotmail.com>
2020-06-15 23:29:00 -0400

    My First change.

--- tree.py
+++ tree.py
@@ -1,22 +1,63 @@
 from collections import deque
 
 
-data = {
-    'n1': {
+def dump(data, indent=None):
+    indent = indent if indent else '.'
+
+    print('-------------------------------------------------------------------------------------------------------')
+    if data:
+        def walk(_data, count):
+            count += 1
+            for key, value in _data.items():
+                if isinstance(value, dict):
+                    print(indent * count, key)
+                    walk(value, count)
+                else:
+                    if isinstance(value, str):
+                        value = f'"{value}"'
+                    print(indent * count, key, f'value={value}')
+
+        walk(data, 0)
+    else:
+        print(' (No Data)')
+
+    print('-------------------------------------------------------------------------------------------------------')
+
+
+cfg = (
+    {'n1': {
         'text': 'text n1',
         'i1': {'text': 'test i 1'}
-    },
-    'n2': {
+    }},
+    {'n2': {
         'text': 'text n1',
         'i1': {'text': 'test i 1'}
-    },
-}
+    }},
+)
 
-que = deque()
-for k, v in data.items():
-    que.append({k:v})
+START = 0
+END = -1
 
-for idx, item in enumerate(que):
-    print(idx, item)
 
-# This is my change!
+class Tree(deque):
+    def __init__(self, parent, data):
+        super().__init__()
+
+        self.children = deque()
+        for item in data:
+            self.add(parent, **item)
+
+    def add(self, parent, index=END, **data):
+        if not parent:
+            if index == END:
+                self.children.append(data)
+            else:
+                self.children.insert(index, **data)
+
+
+t = Tree('', cfg)
+
+t.add('', **{'test': 123})
+
+for item in t.children:
+    print(item)
