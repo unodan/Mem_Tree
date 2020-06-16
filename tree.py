9ea1dae4ac0e55865b84dd6fd588460bb9dbed6f
dan <danhuckson@hotmail.com>
2020-06-16 12:32:01 -0400

    Added base functionality.

--- tree.py
+++ tree.py
@@ -24,49 +24,107 @@ def dump(data, indent=None):
     print('-------------------------------------------------------------------------------------------------------')
 
 
+cfg1 = (
+    {
+        'text': 'node 1',
+        'children': (
+            {
+                'text': 'node 2',
+                'children': (
+                    {'text': 'testing s1'},
+                ),
+            },
+        )
+    },
+)
+
+
 cfg = (
-    {'n1': {
-        'text': 'text n1',
-        'i1': {'text': 'test i 1'}
-    }},
-    {'i2': 'text i1'},
-    {'n2': {
-        'text': 'text n1',
-        'i1': {'text': 'test i 1'}
-    }},
+    {
+        'text': 'Node 1',
+        'children': (
+            {
+                'text': 'Leaf n1'
+            },
+            {
+                'text': 'Leaf n2'
+            },
+        )
+    },
+    {
+        'text': 'Leaf 1',
+    },
+    {
+        'text': 'Leaf 2',
+    },
 )
 
 START = 0
 END = -1
 
 
-class Tree(deque):
-    def __init__(self, data):
+def has_nodes(obj):
+    if 'children' in obj:
+        return True
+    return False
+
+
+class Leaf:
+    def __init__(self, parent, data):
         super().__init__()
-        self.populate('', data)
+        self.parent = parent
+        self.text = data['text']
 
-    def add(self, parent, index=END, **data):
-        if not parent:
-            if index == END:
-                self.append(data)
+
+class Node(deque):
+    def __init__(self, parent, data):
+        super().__init__()
+        self.text = None
+        self.parent = parent
+        self.children = deque()
+
+        self.populate(data)
+
+    def __len__(self):
+        return len(self.children)
+
+    @staticmethod
+    def has_children(item):
+        return True if 'children' in item else False
+
+    @staticmethod
+    def is_node(item):
+        return True if isinstance(item, Node) else False
+
+    def populate(self, data):
+        for itm in data:
+            if 'children' in itm:
+                self.text = itm['text']
+                n = Node(self, itm.pop('children', ()))
+                n.text = itm['text']
+                self.children.append(n)
             else:
-                self.insert(index, **data)
+                self.children.append(Leaf(self, itm))
 
-    def populate(self, parent, data):
-        def walk(_node):
-            for k, x in _node.items():
-                if isinstance(x, dict):
-                    print('node:', k)
-                else:
-                    print('leaf:', k, x)
+    def get_children(self, parent=None):
+        if not parent:
+            return self.children
 
-        for node in data:
-            walk(node)
+
+class Tree(Node):
+    def __init__(self, data):
+        super().__init__(None, data)
 
 
 def main():
     t = Tree(cfg)
-    t.add('', **{'test': 123})
+    for x in t.get_children():
+        if t.is_node(x):
+            print(x.text)
+            for c in x.get_children():
+                print('\t', c.text)
+        else:
+            print(x.text)
 
 
 if __name__ == '__main__':
